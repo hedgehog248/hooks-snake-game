b12e1d8d44bc9451706d5b2c4a592be7e62dee24
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-15 00:44:19 +0900

    ランダムにエサを表示する記述

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,10 @@
+const getFoodPosition = (fieldSize) => {
+  // fieldSizeの一番外側から1マス内側でランダムな座標を取得
+  const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+  const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+  return { x, y };
+}
+
 export const initFields = (fieldSize, initialPosition) => {
   const fields = []
   for (let i = 0; i < fieldSize; i++) {
@@ -6,5 +13,8 @@ export const initFields = (fieldSize, initialPosition) => {
   }
   fields[initialPosition.y][initialPosition.x] = 'snake';
 
-  return fields
+  const food = getFoodPosition(fieldSize);
+  fields[food.y][food.x] = 'food';
+
+  return fields // 作成した配列を返却
 }
