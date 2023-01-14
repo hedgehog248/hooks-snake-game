a6bf159825c250f5f7f3a3ad8b56a15e08d41c98
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-15 00:49:17 +0900

    スネークとエサの位置がかぶらないようにする修正

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,25 @@
-const getFoodPosition = (fieldSize) => {
-  // fieldSizeの一番外側から1マス内側でランダムな座標を取得
-  const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
-  const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
-  return { x, y };
+const getFoodPosition = (fieldSize, excludes) => {
+  while (true) {
+    // fieldSizeの一番外側から1マス内側でランダムな座標を取得
+    const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+    const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+    const conflict = excludes.some(item => item.x === x && item.y === y);
+
+    if (!conflict) {
+      return { x, y };
+    }
+  }
 }
 
-export const initFields = (fieldSize, initialPosition) => {
+export const initFields = (fieldSize, snake) => {
   const fields = []
   for (let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill('')
     fields.push(cols)
   }
-  fields[initialPosition.y][initialPosition.x] = 'snake';
+  fields[snake.y][snake.x] = 'snake';
 
-  const food = getFoodPosition(fieldSize);
+  const food = getFoodPosition(fieldSize, [snake]);
   fields[food.y][food.x] = 'food';
 
   return fields // 作成した配列を返却
