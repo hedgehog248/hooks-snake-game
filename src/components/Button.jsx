25ef4ea0927805b5ccd7c58e24a98a1a644e09ba
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-16 03:02:57 +0900

    ボタンのスタイリング

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,10 +7,10 @@ const Button = ({ status, onStart, onStop, onRestart }) => {
         statusによってボタンの表示を切り替える
         &&の前の条件式がtrueのときだけ後ろの処理が実行される
        */}
-      {status === 'gameover' && <button onClick={onRestart}>gameover</button>}
-      {status === 'init' && <button onClick={onStart}>start</button>}
-      {status === 'suspended' && <button onClick={onStart}>start</button>}
-      {status === 'playing' && <button onClick={onStop}>stop</button>}
+      {status === 'gameover' && <button className="btn btn-gameover" onClick={onRestart}>gameover</button>}
+      {status === 'init' && <button className="btn btn-init" onClick={onStart}>start</button>}
+      {status === 'suspended' && <button className="btn btn-suspended" onClick={onStart}>start</button>}
+      {status === 'playing' && <button className="btn btn-playing" onClick={onStop}>stop</button>}
     </div>
   );
 };
