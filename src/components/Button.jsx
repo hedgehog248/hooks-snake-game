eb09da33d4b157e509b12300f8a989b53cd705ac
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-01-17 19:51:29 +0900

    リファクタリング　各コンポーネントのハードコーディングを整理

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GameStatus } from "../constants";
 
 const Button = ({ status, onStart, onStop, onRestart }) => {
   return (
@@ -7,10 +8,10 @@ const Button = ({ status, onStart, onStop, onRestart }) => {
         statusによってボタンの表示を切り替える
         &&の前の条件式がtrueのときだけ後ろの処理が実行される
        */}
-      {status === 'gameover' && <button className="btn btn-gameover" onClick={onRestart}>gameover</button>}
-      {status === 'init' && <button className="btn btn-init" onClick={onStart}>start</button>}
-      {status === 'suspended' && <button className="btn btn-suspended" onClick={onStart}>start</button>}
-      {status === 'playing' && <button className="btn btn-playing" onClick={onStop}>stop</button>}
+      {status === GameStatus.gameover && <button className="btn btn-gameover" onClick={onRestart}>gameover</button>}
+      {status === GameStatus.init && <button className="btn btn-init" onClick={onStart}>start</button>}
+      {status === GameStatus.suspended && <button className="btn btn-suspended" onClick={onStart}>start</button>}
+      {status === GameStatus.playing && <button className="btn btn-playing" onClick={onStop}>stop</button>}
     </div>
   );
 };
